9b8ff9b195f6801c60306d0d4026dae38ab599be
praveenmaurya09 <praveenbhaimaurya@gmail.com>
2023-01-16 13:46:57 +0530

    basic statful widget

---
+++ lib/pages/basicstateful.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int value = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Day 5"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Counter Value $value",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  // Logic for increment the value +1
+                  increment();
+                },
+                child: Icon(Icons.add)),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  // Logic for decrement the value +1
+                  decrement();
+                },
+                child: Icon(Icons.remove)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void increment() {
+    setState(() {
+      value += 1;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      value -= 1;
+    });
+  }
+}
